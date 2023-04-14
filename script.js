4e8553bd5e9a5dbc94530a6de642c12eeaf1b9fd
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-04-14 10:16:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor = (species){
+		this.species = species;
+	}
+getSpecies(){
+	return this.species;
+}
+makeSound(){
+	console.log("The " + this.species + "make a sound.");
+}
+}
+class Cat extends Animal {
+	purr(){
+		console.log("This " + this.species + " purrs.");
+	}
+}
+class Dog extends Animal {
+bark(){
+		console.log("This " + this.species + " barks.");
+}
+}
 
-class Dog extends Animal {}
 
-class Cat extends Animal {}
 
 // Do not change the code below this line
 window.Animal = Animal;
